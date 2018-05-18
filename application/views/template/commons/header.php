cca48ad766b8f6cab9c6c68a3d48be24593b8274
Elias Douglas <edwportela@gmail.com>
2018-05-18 18:05:48 -0300

    ajuste_css_mobile_site

--- application/views/template/commons/header.php
+++ application/views/template/commons/header.php
@@ -7,6 +7,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="<?=base_url('assets/css/w3s.css')?>">
 <link rel="stylesheet" href="<?=base_url('assets/css/mystyle.css')?>">
+<link rel="stylesheet" href="<?=base_url('assets/css/mobile-site.css')?>">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="<?php echo base_url('assets/vendor/sweetalert-master/dist/sweetalert.css');?>">
@@ -21,15 +22,18 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", Arial, Helvetica, sans-serif}
 </style>
 <body class="w3-light-gray">
 
-<div class="w3-container w3-green w3-padding w3-hide-large w3-hide-medium">
+<div class="w3-container w3-green w3-hide-large w3-hide-medium bar-mobile">
   <div class="w3-bar w3-wide">
     <div class="w3-right">
-      <span onclick="$('#menuCelular').toggle()" class="w3-bar-item w3-button w3-mobile w3-hover-pink"><i class="fa fa-bars fa-2x"></i></span>
+      <span onclick="$('#menuCelular').toggle()" class="w3-bar-item w3-button w3-mobile w3-hover-green"><i class="fa fa-bars"></i></span>
     </div>
-    <div id="menuCelular" style="display: none;">
-      <a href="#projects" class="w3-bar-item w3-button w3-mobile w3-border-top w3-padding-16"><i class="fa fa-building-o"></i> Imóveis</a>
+    <div id="menuCelular" class="" style="display: none;">
+      <a href="<?=base_url('imoveis')?>" class="w3-button w3-block"><i class="fa fa-newspaper-o"></i> Cotações</a>
+      <a href="<?=base_url('quem-somos')?>" class="w3-button w3-block"><i class="fa fa-home"></i> Empresa</a>
+      <a href="<?=base_url('contato')?>" class="w3-button w3-block"><i class="fa fa-envelope-o"></i> Contato</a>
+      <!-- <a href="#projects" class="w3-bar-item w3-button w3-mobile w3-border-top w3-padding-16"><i class="fa fa-building-o"></i> Cotações</a>
       <a href="#about" class="w3-bar-item w3-button w3-mobile w3-border-top w3-padding-16"><i class="fa fa-home"></i> Empresa</a>
-      <a href="#contact" class="w3-bar-item w3-button w3-mobile w3-border-top w3-padding-16"><i class="fa fa-envelope-o"></i> Contato</a>
+      <a href="#contact" class="w3-bar-item w3-button w3-mobile w3-border-top w3-padding-16"><i class="fa fa-envelope-o"></i> Contato</a> -->
     </div>
   </div>
 </div>
